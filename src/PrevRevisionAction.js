b7d8cabdd162d9ed092ebc49960696094dfd3c1b
Helder <he7d3r@gmail.com>
2012-10-05 13:26:16 +0000

    Use .find() and remove "oldid" from URL
    Based on
    https://www.mediawiki.org/w/index.php?title=Snippets/Last_revision_action&oldid=590864

--- src/PrevRevisionAction.js
+++ src/PrevRevisionAction.js
@@ -1,27 +1,27 @@
 /**
  * Action link: Last revision diff
  *
- * @source: http://www.mediawiki.org/wiki/Snippets/Last_revision_action
- * @rev: 5
+ * @source: www.mediawiki.org/wiki/Snippets/Last_revision_action
+ * @rev: 7
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/LastRevisionAction.js]] ([[File:User:Helder.wiki/Tools/LastRevisionAction.js]])
  */
 /*jslint browser: true, white: true*/
-/*global jQuery, mediaWiki */
-( function ( $, mw /* , undefined */ ) {
+/*global mediaWiki, jQuery */
+( function ( mw, $ ) {
 'use strict';
 
 $( function ( $ ) {
         // Not on Special pages
         if ( !mw.config.get('wgCanonicalSpecialPageName') ) {
                 var     url,
-                        $plink = $('#t-permalink a');
+                        $plink = $('#t-permalink').find('a');
                 if ( $plink.length ) {
-                        url = $plink.attr('href').replace( '&oldid=', '&diff=prev&oldid=' );
+                        url = $plink.attr('href').replace( '&oldid=', '&diff=' );
                 } else {
                         url = mw.util.wikiGetlink( mw.config.get( 'wgPageName' ) ) + '?diff=0';
                 }
-                mw.util.addPortletLink( 'p-namespaces', url, '↶', 'ca-lastdiff', 'Mostrar as alterações feitas na última edição' );
+                mw.util.addPortletLink( 'p-namespaces', url, '↶', 'ca-lastdiff', 'Comparar com a versão anterior' );
         }
 } );
 
-}( jQuery, mediaWiki ) );
+}( mediaWiki, jQuery ) );
