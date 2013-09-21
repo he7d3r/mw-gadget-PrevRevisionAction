7e1378964498fc8f1d6f3d1abd9140665f877a1d
Helder <he7d3r@gmail.com>
2013-09-21 18:52:44 +0000

    Use the second parameter of mw.util.wikiGetlink
    This was introduced on
    https://gerrit.wikimedia.org/r/#/c/84633/

--- src/PrevRevisionAction.js
+++ src/PrevRevisionAction.js
@@ -2,7 +2,7 @@
  * Action link: Last revision diff
  *
  * @source: www.mediawiki.org/wiki/Snippets/Last_revision_action
- * @rev: 7
+ * @rev: 7a
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/LastRevisionAction.js]] ([[File:User:Helder.wiki/Tools/LastRevisionAction.js]])
  */
 /*jslint browser: true, white: true*/
@@ -18,7 +18,7 @@ $( function ( $ ) {
                 if ( $plink.length ) {
                         url = $plink.attr('href').replace( '&oldid=', '&diff=' );
                 } else {
-                        url = mw.util.wikiGetlink( mw.config.get( 'wgPageName' ) ) + '?diff=0';
+                        url = mw.util.wikiGetlink( mw.config.get( 'wgPageName' ), { diff: 0} );
                 }
                 mw.util.addPortletLink( 'p-namespaces', url, '↶', 'ca-lastdiff', 'Comparar com a versão anterior' );
         }
