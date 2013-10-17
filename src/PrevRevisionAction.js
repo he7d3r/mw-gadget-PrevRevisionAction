53d5fddff6fc9e0e1a116e596cd7a6ecb566986f
Helder <he7d3r@gmail.com>
2013-10-17 12:48:30 +0000

    Use wgCurRevisionId instead of zero

--- src/PrevRevisionAction.js
+++ src/PrevRevisionAction.js
@@ -2,7 +2,7 @@
  * Action link: Last revision diff
  *
  * @source: www.mediawiki.org/wiki/Snippets/Last_revision_action
- * @rev: 7a
+ * @rev: 7b
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/LastRevisionAction.js]] ([[File:User:Helder.wiki/Tools/LastRevisionAction.js]])
  */
 /*jslint browser: true, white: true*/
@@ -11,17 +11,17 @@
 'use strict';
 
 $( function ( $ ) {
-        // Not on Special pages
-        if ( !mw.config.get('wgCanonicalSpecialPageName') ) {
-                var     url,
-                        $plink = $('#t-permalink').find('a');
-                if ( $plink.length ) {
-                        url = $plink.attr('href').replace( '&oldid=', '&diff=' );
-                } else {
-                        url = mw.util.wikiGetlink( mw.config.get( 'wgPageName' ), { diff: 0} );
-                }
-                mw.util.addPortletLink( 'p-namespaces', url, '↶', 'ca-lastdiff', 'Comparar com a versão anterior' );
-        }
+	// Not on Special pages
+	if ( !mw.config.get('wgCanonicalSpecialPageName') ) {
+		var url,
+			$plink = $( '#t-permalink' ).find( 'a' );
+		if ( $plink.length ) {
+			url = $plink.attr( 'href' ).replace( '&oldid=', '&diff=' );
+		} else {
+			url = mw.util.wikiGetlink( mw.config.get( 'wgPageName' ), { diff: mw.config.get( 'wgCurRevisionId', 0 ) } );
+		}
+		mw.util.addPortletLink( 'p-namespaces', url, '↶', 'ca-lastdiff', 'Comparar com a versão anterior' );
+	}
 } );
 
 }( mediaWiki, jQuery ) );
