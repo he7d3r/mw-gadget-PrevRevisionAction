63224f618fd184d5c060e99c04c19cc683fe6f06
Helder <he7d3r@gmail.com>
2014-09-27 20:49:02 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/PrevRevisionAction.js
+++ src/PrevRevisionAction.js
@@ -5,21 +5,21 @@
  * @rev: 9
  */
 ( function ( mw, $ ) {
-'use strict';
+	'use strict';
 
-mw.hook( 'wikipage.content' ).add( function() {
-        // Not on Special pages
-        if ( mw.config.get( 'wgCanonicalSpecialPageName' ) ) {
-                return;
-        }
-        var url = mw.util.getUrl( mw.config.get( 'wgPageName' ) ) +
-                '?diff=' + ( mw.config.get( 'wgRevisionId' ) || mw.config.get( 'wgCurRevisionId' ) ),
-                $link = $('#ca-prevdiff').find('a');
-        if ( $link.length ) {
-                $link.attr( 'href', url );
-        } else {
-                mw.util.addPortletLink( 'p-namespaces', url, '↶', 'ca-prevdiff', 'Show diff to previous edit' );
-        }
-} );
+	mw.hook( 'wikipage.content' ).add( function () {
+		// Not on Special pages
+		if ( mw.config.get( 'wgCanonicalSpecialPageName' ) ) {
+			return;
+		}
+		var url = mw.util.getUrl( mw.config.get( 'wgPageName' ) ) +
+			'?diff=' + ( mw.config.get( 'wgRevisionId' ) || mw.config.get( 'wgCurRevisionId' ) ),
+			$link = $('#ca-prevdiff').find('a');
+		if ( $link.length ) {
+			$link.attr( 'href', url );
+		} else {
+			mw.util.addPortletLink( 'p-namespaces', url, '↶', 'ca-prevdiff', 'Show diff to previous edit' );
+		}
+	} );
 
 }( mediaWiki, jQuery ) );
